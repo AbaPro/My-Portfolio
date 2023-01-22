084ad299b2340f79dfe4065af764f36a3dade7c1
Abanob Raffet <102380881+AbaPro@users.noreply.github.com>
2023-01-22 21:01:32 +0200

    Add files via upload
    dark mode

---
+++ Scripts/script.js
@@ -0,0 +1,14 @@
+
+var isDark
+var icon = document.getElementById("dark-icon");
+
+localStorage.getItem("is-dark") == "true" ? (isDark = true) : (isDark = false);
+
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (isDark) isDark = false;
+    else isDark = true;
+    // Storing the current theme in the browser's local storage
+    localStorage.setItem("is-dark", isDark);
+};
+if (isDark) document.body.classList.toggle("dark-theme");
