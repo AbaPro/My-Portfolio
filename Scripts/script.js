6ebec2a7985551127f0a2f3be0a3db37950c907f
Abanob Raffet <102380881+AbaPro@users.noreply.github.com>
2023-01-24 10:40:55 +0200

    Update script.js
    loading script

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,14 +1,19 @@
-
-var isDark
+var isDark;
+var loader = document.getElementById("loader");
 var icon = document.getElementById("dark-icon");
 
 localStorage.getItem("is-dark") == "true" ? (isDark = true) : (isDark = false);
 
 icon.onclick = function () {
-    document.body.classList.toggle("dark-theme");
-    if (isDark) isDark = false;
-    else isDark = true;
-    // Storing the current theme in the browser's local storage
-    localStorage.setItem("is-dark", isDark);
+  document.body.classList.toggle("dark-theme");
+  if (isDark) isDark = false;
+  else isDark = true;
+  // Storing the current theme in the browser's local storage
+  localStorage.setItem("is-dark", isDark);
 };
+
 if (isDark) document.body.classList.toggle("dark-theme");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
